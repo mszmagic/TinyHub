7296d3fdecd6db812f735e950083dbb93b40fff7
Shunzhe <git@mszmagic.com>
2020-08-08 17:05:15 -0400

    Minor updates
    - Tests removed
    - gitignore added

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "TinyHubView",
             dependencies: []),
-        .testTarget(
-            name: "TinyHubViewTests",
-            dependencies: ["TinyHubView"]),
     ]
 )
